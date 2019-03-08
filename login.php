2a8247182e6c7cae054d1e5202b6428948a0bb16
tejveersingh1997 <tejveersingh1997@gmail.com>
2019-03-08 13:26:46 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,70 +1,3 @@
-Last login: Fri Mar  8 00:47:08 on ttys001
-vpn-10-192:~ tejveersingh$ ssh -l ts355 afs1.njit.edu
-Password: 
-Last login: Fri Mar  8 00:47:23 2019 from 10.192.224.15
-Starting /afs/cad/u/t/s/ts355/.bash_profile ... standard AFS bash profile
-
-========================
-==> AFS disk quota in your home directory [ /afs/cad/u/t/s/ts355 ] :	
-Volume Name                    Quota       Used %Used   Partition
-user.ts355                   1000000      35682    4%         83%  
-
-		Quota = 1.00   Gbytes    
-
-		Used  = 0.03   Gbytes    
-
-		  Note: "Partition" refers to the percentage use of the disk
-			 that the AFS volume is on.
-	
-		
-	To free up disk space, including Firefox & Chrome cache, enter : 
-			
-			clean.home
-			(/usr/ucs/bin/clean.home)
-
-Calculating Firefox cache usage ...
-Put a file named "NoCalcCache" in your home directory to suppress.
-du: cannot access ‘./BACKUP-AFS.ACCOUNT/BACKUP-AFS.ACCOUNT’: No such device
- Your current Firefox cache uses : 70 Mbytes      
-
-========================
-
-On host afsconnect1.njit.edu :
-	 12:49:59 up 18 days, 16:44, 17 users,  load average: 0.14, 0.12, 0.15
-
-=== === === Your Kerberos ticket and AFS token status === === ===
-Kerberos :  Renew until 04/07/2019 13:49:57, Flags: FRI
-AFS      :  User's (AFS ID 94421) tokens for afs@cad.njit.edu [Expires Mar  8 22:49]
-
-
-=== === === Start Python Information === === ===
-1. The default python is Anaconda Python 3
-
-2. To instead use Anaconda Python 2 :
-
-	echo 'module load python2' > ~/.modules
-
-	Then log out / log in 
-=== === === End Python Information === === ===
-
-Loading [ python3.6 java10 matlab/2018b ] modules ...
-Currently Loaded Modulefiles:
-  1) python3.6       2) java10/10.0.2   3) matlab/2018b
-
-
-To see your aliases, enter "alias"
-
-afsconnect1-41 ~ >: cd public_html
-afsconnect1-42 public_html >: ls
-ads/        instructor_answers.php   login_backend.php  login_valid.js
-cs490/      instructor_maketest.php  login.css          student_review.php
-db.php      instructor_view.php      login_front.js     student_test.php
-download/   iview_backend.php        login_middle.php   student_view.php
-index.html  iview_front.js           login.php          UPLOADS/
-afsconnect1-43 public_html >: nano instructor_view.php
-afsconnect1-44 public_html >: nano login.php
-
-  GNU nano 2.3.1                                                File: login.php                                                                                                      
 
 <!DOCTYPE html>
 <html>
@@ -117,6 +50,70 @@ button {
   padding: 20px 30px;
   margin: 8px 0;
   border: none;
+  cursor: pointer;
+  width: 60%;
+}
 
-^G Get Help                   ^O WriteOut                   ^R Read File                  ^Y Prev Page                  ^K Cut Text                   ^C Cur Pos
-^X Exit                       ^J Justify                    ^W Where Is                   ^V Next Page                  ^U UnCut Text                 ^T To Spell
+button:hover {
+  opacity: 0.8;
+}
+
+</style>
+</head>
+
+
+
+<!–– HTML Menu here ––>
+
+
+
+<!–– HTML Login form here ––>
+<center>
+<h1 class="h1"> NJIT E-PROCTOR </h1>
+	
+<body>
+
+<form action="/action_page.php">
+
+        <div class="container">
+
+        <tr class="prop">
+                <td class="name"><label for="login:username"></label></td>
+                <td class="value"><input id="login:username" type="text" name="login:username" class="form-login" size="30" placeholder="Username" required autofocus></td>
+        </tr>
+	<br></br>
+        <tr class="prop">
+                <td class="name"><label for="login:password"></label></td>
+                <td class="value"><input id="login:password" type="password" name="login:password" class="form-login" size="30" placeholder="Password"></td>
+        </tr>
+	<br></br>
+        <tr class="prop">
+                <td class="value"><input id="login:user" type="radio" name="login:user" class="form-login" value="teacher"> Teacher</td>
+                <td class="value"><input id="login:user1" type="radio" name="login:user" class="form-login" value="student"> Student</td>
+        </tr>
+	<br></br>
+
+        <!-- -->
+        <div class="button" style="margin: 1em 1em">
+                <input class="form-control" type="button" onclick="ajaxLoginFunction();" value="Login">
+        </div>
+
+</form>
+
+</body>
+
+
+</center>
+
+<!–– Sends MID_PATH variable to login_front.js ––>
+
+<script type="text/javascript">
+        var MID_PATH="https://web.njit.edu/~vpp29/";
+</script>
+
+<div id="ajaxDiv"></div>
+<div id="ajaxDiv2" style="display: none;"></div>
+
+</html>
+
+
