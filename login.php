117c12d0ef9f4d8459caa9e52a222dcdb637644f
tejveersingh1997 <tejveersingh1997@gmail.com>
2019-03-11 17:00:42 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,61 +1,63 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-
+<meta charset="utf-8">
+<meta name="robots" content="noindex,nofollow" />
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalab$
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-touch-fullscreen" content="yes">
+<meta name="format-detection" content="telephone=no">
+<meta name="mobile-web-app-capable" content="yes">
+						 
+						 
 <!–– Links with login_front.js ––>
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="login_front.js" type="text/JavaScript"></script>
 
-
-<!–– CSS style is inserted here––>
-
 <style>
-
-h1 {
-  font-family: Arial, Helvetica, sans-serif;
-  color:White;
-  font-size: 40px;
-}
 
 body {
   font-family: Arial, Helvetica, sans-serif;
-  color:White;
-  font-size: 8px;
-  background-color: #711313;
+  color:black;
+  font-size: 18px;
+//  background: url("signin.png") no-repeat center center fixed;
+//  background-size: 1000px 650px;
 }
 
 form {
-  position:center;
-        border: 5px solid #f1f1f1;
-        padding: 12px 10px;
-        background-color: rgb(228, 78, 78);
-        width: 60%;
-
+  position: absolute;
+  background-color: rgb(100, 215, 50, 1.0);
+  width: 30%;
+  height: 40%;
+  transform: translate(115%, 94%);
 }
 
 input[type=text], input[type=password] {
   width: 40%;
-  padding: 12px 10px;
-  margin: 8px 0;
+  padding: 12px 12px;
+  margin: 4px 0;
   display: inline-block;
   border: 2px solid #ccc;
   box-sizing: border-box;
 }
 
 button {
-  background-color: red;
-  color: white;
-  padding: 20px 30px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 60%;
+  border-radius: 15px;
+  background: #73AD21;
+  padding: 20px;
+  width: 200px;
+  height: 150px;
 }
 
 button:hover {
   opacity: 0.8;
+}
+
+ajaxDisplay, ajaxHiddenDisplay{
+font-family: Arial, Helvetica, sans-serif;
+  color:red;
+  font-size: 8px;
 }
 
 </style>
@@ -64,19 +66,16 @@ button:hover {
 
 
 <!–– HTML Menu here ––>
-
-
-
+				
 <!–– HTML Login form here ––>
 <center>
-<h1 class="h1"> NJIT E-PROCTOR </h1>
-	
+
 <body>
 
 <form action="/action_page.php">
 
         <div class="container">
-
+        <br></br>
         <tr class="prop">
                 <td class="name"><label for="login:username"></label></td>
                 <td class="value"><input id="login:username" type="text" name="login:username" class="form-login" size="30" placeholder="Username" required autofocus></td>
@@ -92,17 +91,16 @@ button:hover {
                 <td class="value"><input id="login:user1" type="radio" name="login:user" class="form-login" value="student"> Student</td>
         </tr>
 	<br></br>
-
-        <!-- -->
-        <div class="button" style="margin: 1em 1em">
-                <input class="form-control" type="button" onclick="ajaxLoginFunction();" value="Login">
+ <!-- -->
+        <div class="button">
+                <input class="form-control" type="button" onclick="ajaxLoginFunction();" value="Sign In">
         </div>
-
 </form>
 
+<div id=ajaxDisplay></div>
+<div id=ajaxHiddenDisplay></div>
+
 </body>
-
-
 </center>
 
 <!–– Sends MID_PATH variable to login_front.js ––>
@@ -111,9 +109,7 @@ button:hover {
         var MID_PATH="https://web.njit.edu/~vpp29/";
 </script>
 
-<div id="ajaxDiv"></div>
-<div id="ajaxDiv2" style="display: none;"></div>
-
 </html>
+
 
 
